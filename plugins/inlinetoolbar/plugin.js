26d3ce62391be23f49debb68762ff7791d33f16c
Wojciech Wozniak <wwozniak92@gmail.com>
2017-09-28 11:47:38 +0200

    CSS styling for inline toolbar

--- plugins/inlinetoolbar/plugin.js
+++ plugins/inlinetoolbar/plugin.js
@@ -26,10 +26,17 @@
 		this.inlineToolbar = new CKEDITOR.ui.inlineToolbarView( editor );
 	};
 
+	var stylesLoaded = false;
+
 	CKEDITOR.plugins.add( 'inlinetoolbar', {
 		requires: 'balloonpanel',
 		init: function() {
+			if ( !stylesLoaded ) {
+				CKEDITOR.document.appendStyleSheet( this.path + 'skins/' + CKEDITOR.skinName + '/inlinetoolbar.css' );
+				stylesLoaded = true;
+			}
 			CKEDITOR.ui.inlineToolbarView.prototype = CKEDITOR.tools.extend( {}, CKEDITOR.ui.balloonPanel.prototype );
+			CKEDITOR.ui.inlineToolbar.prototype.templateDefinitions.panel = CKEDITOR.ui.inlineToolbar.prototype.templateDefinitions.panel.replace( 'cke_balloon', 'cke_inlinetoolbar' );
 			/**
 			 * build inline toolbar DOM representation.
 			 */
@@ -111,4 +118,17 @@
 			};
 		}
 	} );
+<<<<<<< HEAD
+=======
+
+	CKEDITOR.ui.inlineToolbar = function( editor, definition ) {
+		var defParams = CKEDITOR.tools.extend( definition || {}, {
+			width: 'auto',
+			triangleWidth: 10,
+			triangleHeight: 10
+		} );
+		CKEDITOR.ui.balloonPanel.call( this, editor, defParams );
+	};
+
+>>>>>>> CSS styling for inline toolbar
 }() );
